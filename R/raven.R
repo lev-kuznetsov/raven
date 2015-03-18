7650552042c3701693ac37eadf3ec45d9abbaf1e
lev-kuznetsov <lev.v.kuznetsov@gmail.com>
2015-03-18 16:25:50 -0400

    Fix package comment

--- R/raven.R
+++ R/raven.R
@@ -12,7 +12,7 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
-#' Dependency injection framework
+#' R versioned package environment support
 #'
 #' @importFrom jsonlite fromJSON
 #' @importFrom jsonlite toJSON
